c0914311ba9c95a702fa671f504b89599c933947
oDs1337 <tobiasz.contact@gmail.com>
2025-08-06 08:51:07 +0200

    add config service and config interface

---
+++ src/app/services/config-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AppConfig } from '../shared/interfaces/AppConfig';
+import appConfigJson from '../../../config.json' assert { type: 'json' };
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+  private config: AppConfig = appConfigJson;
+
+  loadConfig(): Observable<AppConfig> {
+    return of(this.config);
+  }
+
+  get<T extends keyof AppConfig>(key: T): AppConfig[T] {
+    if (!this.config) {
+      throw new Error('unable to load config');
+    } else {
+      return this.config[key];
+    }
+  }
+}
---
+++ src/app/shared/interfaces/AppConfig.ts
@@ -0,0 +1,10 @@
+export interface AppConfig {
+  appName: string;
+  companyName: string;
+  logoUrl: string;
+  defaultTheme: string;
+  themes: string[];
+  accessibilitySupport: boolean;
+  defaultLanguage: string;
+  multipleLanguagesSupport: boolean;
+}
