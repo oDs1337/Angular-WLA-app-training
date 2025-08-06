c38911fc6a671c7bc013a46983bb0a72d4802db2
oDs1337 <tobiasz.contact@gmail.com>
2025-08-06 08:56:21 +0200

    Provide config to app initializer, add http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { ConfigService } from '@services/config-service';
+import { firstValueFrom } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    provideAppInitializer(() => {
+      const configService: ConfigService = inject(ConfigService);
+      return firstValueFrom(configService.loadConfig());
+    }),
+  ],
 };
